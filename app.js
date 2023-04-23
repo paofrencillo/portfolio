f55e6549f032e860b832d263e4eea3af2ee66cf5
paopaolopaoloo <simonruzzel@gmail.com>
2023-04-23 22:32:15 +0800

    about u

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-function fadeIn(link) {}
+$window = $(window);
+$window.scroll(function () {
+  if ($window.scrollTop() >= $("#home").offset().top - 150) {
+    $(".Navbar-links").children().removeClass("active");
+    $(".Navbar-links").children().eq(0).addClass("active");
+  }
+  if ($window.scrollTop() >= $("#about").offset().top - 150) {
+    $(".Navbar-links").children().removeClass("active");
+    $(".Navbar-links").children().eq(1).addClass("active");
+  }
+});
 
-function fadeOut(link) {}
+function changeOption(div) {
+  $(".Eeo-inner").children().removeClass("selected");
+  $(div).first().addClass("selected");
+}
---
+++ spin.js
@@ -0,0 +1,139 @@
+$(document).ready(function () {
+  $("#rotate1")
+    .get(0)
+    .animate([{ transform: "rotate(0deg)" }, { transform: "rotate(360deg)" }], {
+      duration: 10000,
+      easing: "linear",
+      iterations: Infinity,
+      fill: "forwards",
+    });
+
+  $("#lang-img1")
+    .get(0)
+    .animate(
+      [{ transform: "rotate(0deg)" }, { transform: "rotate(-360deg)" }],
+      {
+        duration: 10000,
+        easing: "linear",
+        iterations: Infinity,
+        fill: "forwards",
+      }
+    );
+
+  $("#lang-img2")
+    .get(0)
+    .animate(
+      [{ transform: "rotate(0deg)" }, { transform: "rotate(-360deg)" }],
+      {
+        duration: 10000,
+        easing: "linear",
+        iterations: Infinity,
+        fill: "forwards",
+      }
+    );
+
+  $("#lang-img3")
+    .get(0)
+    .animate(
+      [{ transform: "rotate(0deg)" }, { transform: "rotate(-360deg)" }],
+      {
+        duration: 10000,
+        easing: "linear",
+        iterations: Infinity,
+        fill: "forwards",
+      }
+    );
+
+  $("#lang-img4")
+    .get(0)
+    .animate(
+      [{ transform: "rotate(0deg)" }, { transform: "rotate(-360deg)" }],
+      {
+        duration: 10000,
+        easing: "linear",
+        iterations: Infinity,
+        fill: "forwards",
+      }
+    );
+
+  $("#lang-img5")
+    .get(0)
+    .animate(
+      [{ transform: "rotate(0deg)" }, { transform: "rotate(-360deg)" }],
+      {
+        duration: 10000,
+        easing: "linear",
+        iterations: Infinity,
+        fill: "forwards",
+      }
+    );
+
+  $("#lang-img6")
+    .get(0)
+    .animate(
+      [{ transform: "rotate(0deg)" }, { transform: "rotate(-360deg)" }],
+      {
+        duration: 10000,
+        easing: "linear",
+        iterations: Infinity,
+        fill: "forwards",
+      }
+    );
+
+  $("#rotate2")
+    .get(0)
+    .animate([{ transform: "rotate(0deg)" }, { transform: "rotate(360deg)" }], {
+      duration: 10000,
+      easing: "linear",
+      iterations: Infinity,
+      fill: "forwards",
+    });
+
+  $("#fw-img1")
+    .get(0)
+    .animate(
+      [{ transform: "rotate(0deg)" }, { transform: "rotate(-360deg)" }],
+      {
+        duration: 10000,
+        easing: "linear",
+        iterations: Infinity,
+        fill: "forwards",
+      }
+    );
+
+  $("#fw-img2")
+    .get(0)
+    .animate(
+      [{ transform: "rotate(0deg)" }, { transform: "rotate(-360deg)" }],
+      {
+        duration: 10000,
+        easing: "linear",
+        iterations: Infinity,
+        fill: "forwards",
+      }
+    );
+
+  $("#fw-img3")
+    .get(0)
+    .animate(
+      [{ transform: "rotate(0deg)" }, { transform: "rotate(-360deg)" }],
+      {
+        duration: 10000,
+        easing: "linear",
+        iterations: Infinity,
+        fill: "forwards",
+      }
+    );
+
+  $("#fw-img4")
+    .get(0)
+    .animate(
+      [{ transform: "rotate(0deg)" }, { transform: "rotate(-360deg)" }],
+      {
+        duration: 10000,
+        easing: "linear",
+        iterations: Infinity,
+        fill: "forwards",
+      }
+    );
+});
