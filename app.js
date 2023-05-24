032f8cee457361698955e49531bbbf7add138baf
paopaolopaoloo <simonruzzel@gmail.com>
2023-05-24 19:54:08 +0800

    add responsivess adesign

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-if (document.body.clientWidth > 768) {
-  console.log("!!!!!");
-  $(".navbar__links__wrapper").removeClass("nav__responsive");
-  $("body").css("overflow", "auto");
-}
+$(window).resize(() => {
+  if (document.body.clientWidth > 768) {
+    $(".navbar__links__wrapper").removeClass("nav__responsive");
+    $("body").css("overflow", "auto");
+  }
+});
 
 $("#hamburger").on("click", () => {
   console.log("???????");
   $(".navbar__links__wrapper").addClass("nav__responsive");
   $("body").css("overflow", "hidden");
+});
+
+$("#close").on("click", () => {
+  $(".navbar__links__wrapper").removeClass("nav__responsive");
+  $("body").css("overflow", "auto");
 });
